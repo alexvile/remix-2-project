c27746c5997a24a5b36982d9de6f79aaf46eea9f
Alexey <alexei20293@yahoo.com>
2023-03-27 22:12:04 +0300

    add(feature): load 5 new jokes

--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -1,23 +1,31 @@
-import { Outlet, Link } from "@remix-run/react";
+import { Outlet, Link, useLoaderData } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
 import stylesUrl from "~/styles/jokes.css";
-
+import { json } from "@remix-run/node";
+import { db } from "~/utils/db.server";
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: stylesUrl }];
 };
 
+export const loader = async () => {
+  return json({
+    jokeListItems: await db.joke.findMany({
+      take: 5,
+      select: { id: true, name: true },
+      orderBy: { createdAt: "desc" },
+    }),
+  });
+};
+
 export default function JokesRoute() {
-    return (
-      <div className="jokes-layout">
+  const data = useLoaderData<typeof loader>();
+  return (
+    <div className="jokes-layout">
       <header className="jokes-header">
         <div className="container">
           <h1 className="home-link">
-            <Link
-              to="/"
-              title="Remix Jokes"
-              aria-label="Remix Jokes"
-            >
+            <Link to="/" title="Remix Jokes" aria-label="Remix Jokes">
               <span className="logo">🤪</span>
               <span className="logo-medium">J🤪KES</span>
             </Link>
@@ -30,9 +38,11 @@ export default function JokesRoute() {
             <Link to=".">Get a random joke</Link>
             <p>Here are a few more jokes to check out:</p>
             <ul>
-              <li>
-                <Link to="some-joke-id">Hippo</Link>
-              </li>
+              {data.jokeListItems.map((joke) => (
+                <li key={joke.id}>
+                  <Link to={joke.id}>{joke.name}</Link>
+                </li>
+              ))}
             </ul>
             <Link to="new" className="button">
               Add your own
@@ -44,5 +54,5 @@ export default function JokesRoute() {
         </div>
       </main>
     </div>
-      );
+  );
 }
